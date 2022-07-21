35e8f1e64150a6c33b46ff1b63f6b6b9ff961917
HP <HP@DESKTOP-G4J7E8F>
2022-07-21 18:07:00 +0530

    Add project files.

---
+++ ViewStateDemo/Sample.aspx
@@ -0,0 +1,20 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true"  CodeBehind="Sample.aspx.cs" Inherits="ViewStateDemo.Sample" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Button ID="btnHit" runat="server" Text="Hit count" OnClick="btnHit_Click" EnableViewState="false"/>
+        </div>
+
+        <div>
+            <asp:Button ID="htnOpen" runat="server" Text="Open new page" OnClick="htnOpen_Click" PostBackUrl="nextPage.aspx"/>
+        </div>
+    </form>
+</body>
+</html>
---
+++ ViewStateDemo/Sample.aspx.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ViewStateDemo
+{
+    public partial class Sample : System.Web.UI.Page
+    {
+       
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnHit_Click(object sender, EventArgs e)
+        {
+            int count = 0;
+            if (ViewState["Counter"] == null)
+                count += 1;
+            else
+                count = ((int)ViewState["Counter"] )+ 1;
+            ViewState["Counter"] = count;
+
+            Response.Write("Counter value is = " + count);
+
+        }
+
+        protected void htnOpen_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
